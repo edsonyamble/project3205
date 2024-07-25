afbf05de8a02e5835fd5dff8694736c59488eb0a
Edson Yamble <edsonyamble2@gmail.com>
2024-07-25 08:54:27 +0700

    add login

--- vue-3205/vue_3205/src/components/formGet.vue
+++ vue-3205/vue_3205/src/components/formGet.vue
@@ -1,33 +1,24 @@
 <template>
-  <div id="app">
+  <div>
     <h1>get</h1>
-     {{ info }}
   </div>
 </template>
 <style scoped></style>
 <script>
 import axios from 'axios'
 export default {
-  name: 'GetFormAxios',
-  data() {
-    return {
-     info: null
-    }
-  },
-  methods: {
-    mounted() {
-      axios
-        .get(`http://localhost:3500/auth`)
-        .then((res) => {
-          console.log(res.data)
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-        .finally(() => {
-          //Perform action in always
-        })
-    }
+  mounted() {
+    axios
+      .get(`http://localhost:3500/form`)
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+      .finally(() => {
+        //Perform action in always
+      })
   }
 }
 </script>
